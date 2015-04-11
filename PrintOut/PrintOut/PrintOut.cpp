30fc2badc807d66088ce0c9cf808329f23c75bf5
luoxixuan <1139720869@qq.com>
2015-04-11 12:11:42 +0800

    create
    create project

---
+++ PrintOut/PrintOut/PrintOut.cpp
@@ -0,0 +1,26 @@
+// PrintOut.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+void printOut(int n)
+{
+	if (n >= 10){
+		printOut(n / 10);
+		cout << endl;
+	}
+		
+	cout << n % 10;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printOut(789654);
+	getchar();
+	return 0;
+}
+
+
+
